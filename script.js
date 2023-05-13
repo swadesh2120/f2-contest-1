b4053c123e6250769a21ba1a3e48638709d296d6
Swadesh Moharana <swadesh2120@gmail.com>
2023-05-13 20:17:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,43 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+   arr.map(function(student){
+      if( student.marks > 50 ){
+        console.log(student);
+      }
+    });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+   arr.forEach(function(student){
+      if(student.marks > 50 ){
+        console.log(student)
+      }
+    });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newStudent = {id:4, name:"susan", age:"20" , marks:45};
+    arr.push(newStudent);
+    console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  let failed_Students = arr.filter((student) => student.marks < 50)
+   console.log(failed_Students);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let new_Arr = [
+      { id: 4, name: "ovaid", age: "24", marks: 90 },
+      { id: 5, name: "suhaib", age: "16", marks: 50 },
+      { id: 6, name: "tafim", age: "16", marks: 50 },
+    ];
+  
+    let concatenatedArr = arr.concat(new_Arr);
+    console.log(concatenatedArr);
 }
